d5ce98ba5ecde776da17a26f18fa1eded4d3974e
un-f0rgiven <barr85@mail.ru>
2024-06-05 21:50:45 +1000

    update file

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,8 @@ from gendiff import generate_diff
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration\
+                                      files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
@@ -14,7 +15,7 @@ def main():
     args = parser.parse_args()
 
     print(generate_diff(args.first_file, args.second_file))
-    
+
 
 if __name__ == '__main__':
     main()
--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -1,15 +1,18 @@
 import json
 
+
 def generate_diff(filepath1, filepath2):
     file1 = json.load(open(str(filepath1)))
     file2 = json.load(open(str(filepath2)))
     keys1 = list(file1.keys())
     keys2 = list(file2.keys())
-    # print(keys1, keys2) # ['host', 'timeout', 'proxy', 'follow'] ['timeout', 'verbose', 'host']
+    # print(keys1, keys2)
+    # ['host', 'timeout', 'proxy', 'follow'] ['timeout', 'verbose', 'host']
     diff_set = set(keys1) & set(keys2)
     set1 = set(keys1) - set(keys2)
     set2 = set(keys2) - set(keys1)
-    # print(diff_set, set1, set2) # {'timeout', 'host'} {'follow', 'proxy'} {'verbose'}
+    # print(diff_set, set1, set2)
+    # {'timeout', 'host'} {'follow', 'proxy'} {'verbose'}
     _f_lst = []
     for i in diff_set:
         if file1[i] == file2[i]:
@@ -21,19 +24,21 @@ def generate_diff(filepath1, filepath2):
         _f_lst.append([1, i, file1[i]])
     for i in set2:
         _f_lst.append([2, i, file2[i]])
-    # print(_f_lst)   # [[0, 'host', 'hexlet.io'], [1, 'timeout', 50], [2, 'timeout', 20], 
-                    # [1, 'follow', False], [1, 'proxy', '123.234.53.22'], [2, 'verbose', True]]
-    _s_lst = sorted(_f_lst, key=lambda x: (x[1]))     # [[1, 'follow', False], [0, 'host', 'hexlet.io'], 
-                                                    # [1, 'proxy', '123.234.53.22'], [1, 'timeout', 50], 
-                                                    # [2, 'timeout', 20], [2, 'verbose', True]]
+    # print(_f_lst)
+    # [[0, 'host', 'hexlet.io'], [1, 'timeout', 50], [2, 'timeout', 20],
+    # [1, 'follow', False], [1, 'proxy', '123.234.53.22'], [2, 'verbose', True]]
+    # _s_lst = sorted(_f_lst, key=lambda x: (x[1]))
+    # [[1, 'follow', False], [0, 'host', 'hexlet.io'],
+    # [1, 'proxy', '123.234.53.22'], [1, 'timeout', 50],
+    # [2, 'timeout', 20], [2, 'verbose', True]]
     result = ''
-    for s, k, v in _s_lst:
-        if s == 0:
-            s = ' '
-        if s == 1:
-            s = '-'
-        if s == 2:
-            s = '+'
-        result = result + s + ' ' + k + ': ' + str(v).lower() + '\n'
+    # for s, k, v in _s_lst:
+    #     if s == 0:
+    #         s = ' '
+    #     if s == 1:
+    #         s = '-'
+    #     if s == 2:
+    #         s = '+'
+    #     result = result + s + ' ' + k + ': ' + str(v).lower() + '\n'
 
     return f'{{\n{result}}}'
